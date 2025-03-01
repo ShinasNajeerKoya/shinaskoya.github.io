5f269eeed7aaed1c69f36b4cb6908c834cbc1613
ShinasNajeerKoya <shinasnajeerkoya@gmail.com>
2025-03-01 16:05:04 +0530

    splash screen added, main layout basic setup completed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1344391881"
+    serviceWorkerVersion: "3200250367"
   }
 });
