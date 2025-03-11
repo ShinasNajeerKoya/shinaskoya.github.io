885a1eaa4465f6e367399f072162dccd5945c44a
ShinasNajeerKoya <shinasnajeerkoya@gmail.com>
2025-03-11 22:03:21 +0530

    project details updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3100867391"
+    serviceWorkerVersion: "2669199157"
   }
 });
